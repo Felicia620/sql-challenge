82332163e1609dd1ae95c9dc2086e4448c148f38
Felicia <84341676+Felicia620@users.noreply.github.com>
2021-08-12 20:43:13 -0400

    Add files via upload

---
+++ table_schemata.sql
@@ -0,0 +1,41 @@
+CREATE TABLE departments(
+	dept_no VARCHAR(30) NOT NULL,
+	dept_name VARCHAR(30) NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE titles(
+	title_id VARCHAR(30) NOT NULL,
+	title VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employees(
+	emp_no INT NOT NULL,
+	emp_title VARCHAR(30) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(30) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES dept_emp(emp_no)
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(30) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES dept_emp(emp_no)
+);
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(30) NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES dept_emp(emp_no)
+);
